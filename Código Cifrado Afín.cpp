134cb8538d3c3db75fb5e6e272ea4a89eda27ca2
Leonardoramirezzz <85765075+Leonardoramirezzz@users.noreply.github.com>
2022-04-28 22:31:15 -0500

    Update Código Cifrado Afín.cpp

--- Código Cifrado Afín.cpp
+++ Código Cifrado Afín.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+
+int mod_ (int a, int b) {
+    int q = a / b;
+    int r = a - (q * b);
+    if (r < 0) { q--; r = a - (q * b); }
+    return r;
+}
+
 int EuclidesExtendido(int a, int b, int &x, int &y) {
     if(b == 0) {
        x = 1;
@@ -21,7 +29,7 @@ int inverso(int a, int n)
     int x, y;
     int eucli = EuclidesExtendido(a, n, x, y);
     if (eucli == 1){
-      if(0<=x<=n-1){
+      if(0 <= x <= n-1){
         int resultado = (x % n + n) % n;
         //cout << "inverso es:  " << resultado;
         return resultado;
@@ -36,78 +44,73 @@ int inverso(int a, int n)
 }
 
 int main() {
-  int a, b,n=27, aux,existencia;
-  char lista_letras[27];
-  lista_letras[0] = 'A';
-
-  for(int i=1;i<27;i++){
-    if(i==14){
-      lista_letras[i] = char(35);
-    }
-    else if(i==15){
-      lista_letras[i] = char(79);
-    }
-    else{
-      lista_letras[i]=lista_letras[i-1]+1;
-    }
-    
-  }
+  
+  int a, b, n = 27, aux, existencia;
+  string lista_letras = "ABCDEFGHIJKLMN#OPQRSTUVWXYZ";
   int opcion;
+  
   while(true){
     
     cout << "Ingrese los factores (a,b): " << endl;
     cin >> a;
     cin >> b;
+    
     if(n<0){existencia=0;}  
     else if((a<0)&& (b<0)) {existencia=0;}
     else if(b>=27){existencia=0;}
     else{existencia=inverso(a,n);}
     if(existencia!=0){
+    
   while(true){
-  cout << "Ingrese 1 si quiere cifrar una palabra, 2 si quiere descifrar una palabra, 3 si quiere escoger otros factores (a,b): ";
+  cout << "Ingrese: " << endl << "1 si quiere cifrar una palabra" << endl;
+  cout << "2 si quiere descifrar una palabra" << endl;
+  cout << "3 si quiere escoger otros factores (a,b): " << endl;
   cin >> opcion;
+    
   if(opcion == 1){
-  cout << "Ingrese la palabra que desee cifrar(Ñ=#):" << endl;
+  cout << "Ingrese la palabra que desee cifrar (Ñ=#):" << endl;
   char palabra[100];
   cin >> palabra;
   int cantidad = strlen(palabra);
   int nueva_palabra;
-  for(int i=0;i<cantidad;i++){
-    for(int j=0;j<27;j++){
-      if(palabra[i]==lista_letras[j]){
+  for(int i = 0; i < cantidad; i++){
+    for(int j = 0; j < 27; j++){
+      if(palabra[i] == lista_letras[j]){
         nueva_palabra=j;
       }
-      
     }
-    nueva_palabra=(a*nueva_palabra)+b;
-    nueva_palabra=(nueva_palabra%27);
-    palabra[i]=lista_letras[nueva_palabra];
+    nueva_palabra = ( a * nueva_palabra) + b;
+    nueva_palabra = (nueva_palabra % 27);
+    palabra[i] = lista_letras[nueva_palabra];
   }
   cout << "La palabra ya cifrada es: " << palabra << endl;
   } 
-  else if(opcion==2){
-  cout << "Ingrese la palabra que desee descifrar:" << endl;
+    
+  else if(opcion == 2){
+
+  cout << "Ingrese la palabra que desee descifrar: " << endl;
   char palabra[100];
   cin >> palabra;
   int cantidad = strlen(palabra);
   int nueva_palabra;
-  for(int i=0;i<cantidad;i++){
-    for(int j=0;j<27;j++){
-      if(palabra[i]==lista_letras[j]){
-        nueva_palabra=j;
+
+  for(int i = 0; i < cantidad; i++){
+    for(int j = 0; j < 27; j++){
+      if(palabra[i] == lista_letras[j]){
+        nueva_palabra = j;
       }
-      
     }
-    nueva_palabra=(nueva_palabra-7)*existencia;
-    nueva_palabra=(nueva_palabra%27);
-    palabra[i]=lista_letras[nueva_palabra];
+    
+    nueva_palabra = mod_(mod_(nueva_palabra - b, n) * existencia, n);
+    nueva_palabra = mod_(nueva_palabra, n);  //(nueva_palabra%27);
+    palabra[i] = lista_letras[nueva_palabra];
   }
   cout << "La palabra ya descifrada es: " << palabra << endl;
+
   }
   else{break;}
   }
   }
   else cout << "Ocurrió un error al ingresar los números,ingrese números válidos" << endl;
-  }
-  
+  } 
 }
